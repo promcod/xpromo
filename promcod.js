0d64eecdfad9a05b92706bf1675a6491d7ef54c5
promcod <asgharam890@gmail.com>
2024-07-15 11:36:00 +0330

    Update promcod.js

--- promcod.js
+++ promcod.js
@@ -1,4 +1,16 @@
-function renderHTML(){
-  var html = document.getElementById("writtenHTML").value
-  document.getElementById("preview").innerHTML = html;
+window.onload = function () {
+    var clickableItems = document.getElementsByClassName("clickable");
+    for (var i = 0; i < clickableItems.length; i++) {
+        clickableItems[i].addEventListener("click", function () {
+            toggle_visibility(this.id);
+        });
+    }
+
 }
+
+    function toggle_visibility(id) {
+        var getToggleItem = document.getElementById(id).getAttribute("data-toggle");
+        var e = document.getElementById(getToggleItem);
+ //       alert(e);
+        (e.style.display != "block") ? e.style.display = "block" :    e.style.display = "none";
+    }
